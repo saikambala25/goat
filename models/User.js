95e02ccbb587c0e75d0c2f0f9277abe0eee36948
saikambala25 <saikambala111@gmail.com>
2025-12-05 23:11:53 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,74 +1,33 @@
 // models/User.js
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-
-const cartItemSchema = new mongoose.Schema(
-  {
-    livestockId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Livestock",
-      required: true,
-    },
-    selected: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  { _id: false }
-);
-
-const addressSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    phone: { type: String, required: true },
-    line1: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    pincode: { type: String, required: true },
-  },
-  { _id: false }
-);
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
       required: true,
-      trim: true,
-      minlength: 2,
+      trim: true
     },
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
-      trim: true,
+      trim: true
     },
     password: {
       type: String,
       required: true,
-      minlength: 6,
-    },
-
-    // Saved per-user state for dashboard
-    cart: [cartItemSchema], // [{ livestockId, selected }]
-    wishlist: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Livestock",
-      },
-    ], // array of livestock IDs
-    addresses: [addressSchema], // [{ name, phone, line1, city, state, pincode }]
+      minlength: 6
+    }
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-// Hash password before save if modified
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-
+// Hash password before save
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
@@ -78,9 +37,8 @@ userSchema.pre("save", async function (next) {
   }
 });
 
-// Compare plain password with hashed password
-userSchema.methods.comparePassword = async function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
+userSchema.methods.comparePassword = function (candidate) {
+  return bcrypt.compare(candidate, this.password);
 };
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
